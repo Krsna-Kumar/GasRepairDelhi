f2d1b07746c210a3f01d29c3cac40a5816be5c38
Krishna Kumar <86921469+Krsna-Kumar@users.noreply.github.com>
2024-09-22 12:27:05 +0530

    Testimonial added

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ const navLinks = document.querySelector('.nav-links');
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+const body = document.body,
+                scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+                height = scrollWrap.getBoundingClientRect().height - 1,
+                speed = 0.04;
+
+            var offset = 0;
+
+            body.style.height = Math.floor(height) + "px";
+
+            function smoothScroll() {
+                offset += (window.pageYOffset - offset) * speed;
+
+                var scroll = "translateY(-" + offset + "px) translateZ(0)";
+                scrollWrap.style.transform = scroll;
+
+                callScroll = requestAnimationFrame(smoothScroll);
+            }
+
+            smoothScroll();
